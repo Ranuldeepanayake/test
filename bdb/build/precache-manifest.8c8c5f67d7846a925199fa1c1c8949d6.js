9f26ab68069dca1f37be95ba7d34d53827ad678a
Ranuldeepanayake <ranuldeepanayake@outlook.com>
2019-12-08 22:07:38 +0530

    Uploaded the build

---
+++ bdb/build/precache-manifest.8c8c5f67d7846a925199fa1c1c8949d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72db06bb65fac8f7f92e3cbf66bbeeb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d094d2b0da043e94d0fe",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "23628e6f840c9383c667",
+    "url": "/static/js/2.ef7f2f67.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ef7f2f67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d094d2b0da043e94d0fe",
+    "url": "/static/js/main.472e7a34.chunk.js"
+  },
+  {
+    "revision": "3d86b9defd90371ad070",
+    "url": "/static/js/runtime-main.8a84085a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
